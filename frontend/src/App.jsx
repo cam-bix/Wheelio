6f02039e0f2bec2e09152d27d8364065b8251dab
Braeden Klaehn <btklaehn2006@gmail.com>
2026-07-09 20:03:22 -0400

    Update employee home dashboard and inventory pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import EmployeeBookings from './pages/EmployeeBookings'
-
-//import EmployeeHome from './pages/EmployeeHome'
-//import EmployeeInventory from './pages/EmployeeInventory'
+import EmployeeHome from './pages/EmployeeHome'
+import EmployeeInventory from './pages/EmployeeInventory'
 
 
 import EmployeeStatistics from './pages/EmployeeStatistics'
@@ -19,11 +18,8 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/employee-bookings" element={<EmployeeBookings />} />
         <Route path="/employee-stats" element={<EmployeeStatistics />} />
-
-        {/* 
         <Route path="/employee-inventory" element={<EmployeeInventory />} />
         <Route path="/employee-home" element={<EmployeeHome />} />
-        */}
         <Route path="/Home" element={<Home />} /> 
       </Routes>
     </BrowserRouter>
